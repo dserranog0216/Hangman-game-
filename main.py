62cc56f98f9e61fc0a3c0533bb2ad4ed36da756f
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-12-05 20:43:40 +0000

    Functional Game

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import random
 from words import word_list
-print("You are now playing Hangman. You have 6 lives.")
 wordChosen = random.choice(word_list)
 numOfLetter = len(wordChosen)
 # print(wordChosen)
 Lives = numOfLetter
 # print(numOfLetter)
 seperate_Word= list(wordChosen)
-print(seperate_Word)
+# print(seperate_Word)
+print(f'You are now playing Hangman. You have {Lives} lives.')
+guessed=list()
 for x in str(numOfLetter):
   print("_"*numOfLetter)
   
-print(wordChosen)
+# print(wordChosen)
 
 
 while Lives != 0:
   letterGuessed = input("guess a letter: ")
   letterGuessed
   if letterGuessed in seperate_Word:
+    
     seperate_Word.remove(letterGuessed)
+    guessed.append(letterGuessed)
+    print(guessed)
     if seperate_Word == []:
       print("You win")
+      break
   else:
     Lives += -1
     print(f'Number of lives left: {Lives}')
@@ -30,7 +35,7 @@ while Lives != 0:
 
 if Lives == 0:
   print("YOU LOSE")
-
+  print(f"The word was {wordChosen}.")
 # if lives ==numOfLetterm:
 #   print(YOU WON)
 #   break
